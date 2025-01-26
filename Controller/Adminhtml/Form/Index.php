9268f7c6c407452713b0c11203bbb0b049c2d653
anusham200 <141118850+anusham200@users.noreply.github.com>
2025-01-27 00:55:27 +0530

    Create Index.php

---
+++ Controller/Adminhtml/Form/Index.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Custom\FormFetchPlugin\Controller\Adminhtml\Form;
+
+use Magento\Backend\App\Action;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    protected $resultPageFactory;
+
+    public function __construct(
+        Action\Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        parent::__construct($context);
+        $this->resultPageFactory = $resultPageFactory;
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->set(__('Form Fetch Plugin'));
+        return $resultPage;
+    }
+}
