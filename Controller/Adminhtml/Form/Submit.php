a671873137af7e227f83f7dcc4e25852b659fe9a
anusham200 <141118850+anusham200@users.noreply.github.com>
2025-01-28 00:55:48 +0530

    Update Submit.php

--- Controller/Adminhtml/Form/Submit.php
+++ Controller/Adminhtml/Form/Submit.php
@@ -4,7 +4,6 @@ namespace Custom\FormFetchPlugin\Controller\Adminhtml\Form;
 use Magento\Backend\App\Action;
 use Magento\Framework\App\Request\DataPersistorInterface;
 use Magento\Framework\Controller\ResultFactory;
-use Magento\Framework\App\ObjectManager;
 use Custom\FormFetchPlugin\Model\FormDataFactory;
 
 class Submit extends Action
@@ -12,6 +11,9 @@ class Submit extends Action
     protected $formDataFactory;
     protected $dataPersistor;
 
+    /**
+     * Constructor
+     */
     public function __construct(
         Action\Context $context,
         FormDataFactory $formDataFactory,
@@ -22,27 +24,43 @@ class Submit extends Action
         parent::__construct($context);
     }
 
+    /**
+     * Execute method
+     */
     public function execute()
     {
+        // Retrieve the form data from the request
         $postData = $this->getRequest()->getPostValue();
 
+        // Check if data is not empty
         if (!empty($postData)) {
             try {
+                // Create a new instance of the model
                 $formData = $this->formDataFactory->create();
+
+                // Set the data (ensure keys match the form's input names)
                 $formData->setData([
                     'email' => $postData['email'],
-                    'first_name' => $postData['first_name'],
-                    'last_name' => $postData['last_name'],
-                    'school_name' => $postData['school_name'],
+                    'first_name' => $postData['firstname'], // Adjusted key to match the form field
+                    'last_name' => $postData['lastname'],   // Adjusted key to match the form field
+                    'school_name' => $postData['schoolname'], // Adjusted key to match the form field
                 ]);
+
+                // Save the data to the database
                 $formData->save();
 
+                // Add success message
                 $this->messageManager->addSuccessMessage(__('Form data has been saved successfully.'));
             } catch (\Exception $e) {
+                // Add error message
                 $this->messageManager->addErrorMessage(__('Unable to save form data. Error: %1', $e->getMessage()));
             }
+        } else {
+            // Add warning message if form data is empty
+            $this->messageManager->addErrorMessage(__('No form data to save.'));
         }
 
-        return $this->_redirect('*/*/index'); // Redirect to the form page or another page
+        // Redirect back to the form or another page
+        return $this->_redirect('formfetch/form/index');
     }
 }
