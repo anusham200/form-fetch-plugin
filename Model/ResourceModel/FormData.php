a5446a1ab5b9da5b7858822d29e8465c2d82e959
anusham200 <141118850+anusham200@users.noreply.github.com>
2025-01-29 00:32:43 +0530

    Update FormData.php

--- Model/ResourceModel/FormData.php
+++ Model/ResourceModel/FormData.php
@@ -6,7 +6,7 @@ class FormData extends AbstractDb
 {
     protected function _construct()
     {
-        // Initialize the table and primary key
-        $this->_init('form_fetch_plugin_data', 'email');  // Table name and primary key field
+        $this->_init('form_fetch_plugin_data', 'email'); // table name and primary key
     }
 }
+
