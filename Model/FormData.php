99972b8453ca6e3cb9489493161d97de71286a6b
anusham200 <141118850+anusham200@users.noreply.github.com>
2025-01-29 00:26:34 +0530

    Update FormData.php

--- Model/FormData.php
+++ Model/FormData.php
@@ -1,23 +1,22 @@
-<?php
-
 namespace Custom\FormFetchPlugin\Model;
 
 use Magento\Framework\Model\AbstractModel;
+use Custom\FormFetchPlugin\Model\ResourceModel\FormData as FormDataResource;
 
 class FormData extends AbstractModel
 {
-    protected $_idFieldName = 'email';  // Replace with your primary key field name
-    protected $_table = 'form_fetch_plugin_data';  // Define the database table name where data will be saved
+    protected $_idFieldName = 'email';  // Use primary key as email
+    protected $_table = 'form_fetch_plugin_data';  // The table name
 
-    protected $_dataModel = \Custom\FormFetchPlugin\Model\ResourceModel\FormData::class;
+    protected $_resourceModel = FormDataResource::class;
 
-    // Define getters and setters for your form data attributes
+    // Setters and Getters for the form fields
     public function getEmail() {
         return $this->_getData('email');
     }
 
     public function setEmail($email) {
-        $this->setData('email', $email);
+        return $this->setData('email', $email);
     }
 
     public function getFirstName() {
@@ -25,7 +24,7 @@ class FormData extends AbstractModel
     }
 
     public function setFirstName($firstname) {
-        $this->setData('firstname', $firstname);
+        return $this->setData('firstname', $firstname);
     }
 
     public function getLastName() {
@@ -33,7 +32,7 @@ class FormData extends AbstractModel
     }
 
     public function setLastName($lastname) {
-        $this->setData('lastname', $lastname);
+        return $this->setData('lastname', $lastname);
     }
 
     public function getSchoolName() {
@@ -41,6 +40,6 @@ class FormData extends AbstractModel
     }
 
     public function setSchoolName($schoolname) {
-        $this->setData('schoolname', $schoolname);
+        return $this->setData('schoolname', $schoolname);
     }
 }
