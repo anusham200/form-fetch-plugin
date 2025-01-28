b5813ee52cb8f53a9bda97c477f290d7ec3997da
anusham200 <141118850+anusham200@users.noreply.github.com>
2025-01-28 22:57:48 +0530

    Update Form.php

--- Block/Form.php
+++ Block/Form.php
@@ -3,38 +3,40 @@
 namespace Custom\FormFetchPlugin\Block;
 
 use Magento\Framework\View\Element\Template;
-use Magento\Framework\Data\Form\FormKey;
+use Magento\Framework\Message\ManagerInterface;
 
 class Form extends Template
 {
-    protected $formKey;
+    protected $messageManager;
 
     public function __construct(
         Template\Context $context,
-        FormKey $formKey,
+        ManagerInterface $messageManager,
         array $data = []
     ) {
-        $this->formKey = $formKey;
+        $this->messageManager = $messageManager;
         parent::__construct($context, $data);
     }
 
-    /**
-     * Get the form key HTML
-     *
-     * @return string
-     */
-    public function getFormKeyHtml()
-    {
-        return '<input type="hidden" name="form_key" value="' . $this->formKey->getFormKey() . '">';
-    }
-
-    /**
-     * Get the form action URL
-     *
-     * @return string
-     */
     public function getFormActionUrl()
     {
-        return $this->getUrl('formfetch/Form/Submit'); // Update the route if necessary
+        // Return the URL where the form should submit data
+        return $this->getUrl('formfetch/form/submit');
+    }
+
+    public function getFormKeyHtml()
+    {
+        return $this->getBlockHtml('formkey'); // Retrieve the form key HTML
+    }
+
+    public function getSuccessMessage()
+    {
+        $messages = $this->messageManager->getMessages(true)->getItems();
+        foreach ($messages as $message) {
+            if ($message->getType() === 'success') {
+                return $message->getText();
+            }
+        }
+        return null;
     }
 }
