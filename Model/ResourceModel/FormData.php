d5497a5160432d635745ccf86fb68a9f959c3d13
anusham200 <141118850+anusham200@users.noreply.github.com>
2025-01-29 00:08:58 +0530

    Update FormData.php

--- Model/ResourceModel/FormData.php
+++ Model/ResourceModel/FormData.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Custom\FormFetchPlugin\Model\ResourceModel;
 
 use Magento\Framework\Model\ResourceModel\Db\AbstractDb;
@@ -7,6 +8,7 @@ class FormData extends AbstractDb
 {
     protected function _construct()
     {
-        $this->_init('form_fetch_plugin_data', 'email'); // Table name 'form_fetch_plugin_data', primary key 'email'
+        // Define your table and the primary key field
+        $this->_init('form_fetch_plugin_data', 'email');  // Change to your table name and primary key field
     }
 }
