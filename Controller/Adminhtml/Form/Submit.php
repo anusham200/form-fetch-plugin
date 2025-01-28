2632894cae5d4cf4cc74fb69b269c58e21caeef9
anusham200 <141118850+anusham200@users.noreply.github.com>
2025-01-29 02:08:24 +0530

    Update Submit.php

--- Controller/Adminhtml/Form/Submit.php
+++ Controller/Adminhtml/Form/Submit.php
@@ -1,36 +1,31 @@
+<?php
+
 namespace Custom\FormFetchPlugin\Controller\Form;
 
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\Message\ManagerInterface;
-use Magento\Framework\Logger\Monolog;
 use Custom\FormFetchPlugin\Model\FormDataFactory;
 
 class Submit extends Action
 {
     protected $formDataFactory;
     protected $messageManager;
-    protected $logger;
 
     public function __construct(
         Context $context,
         FormDataFactory $formDataFactory,
-        ManagerInterface $messageManager,
-        Monolog $logger // Add logger here
+        ManagerInterface $messageManager
     ) {
         parent::__construct($context);
         $this->formDataFactory = $formDataFactory;
         $this->messageManager = $messageManager;
-        $this->logger = $logger; // Initialize logger
     }
 
     public function execute()
     {
         // Retrieve form data from POST request
         $postData = $this->getRequest()->getPostValue();
-
-        // Log the form data to check if it's being received
-        $this->logger->info('Form Data:', $postData); // Log form data
 
         if (!empty($postData)) {
             try {
@@ -44,18 +39,18 @@ class Submit extends Action
                 $formData->setSchoolName($postData['schoolname']);
 
                 // Save form data to the database
-                $formData->save();
+                $formData->save();  // Ensure that this method correctly saves the data
 
                 // Add a success message
                 $this->messageManager->addSuccessMessage(__('Form data has been saved successfully.'));
             } catch (\Exception $e) {
                 // Log the error and add an error message
-                $this->logger->error('Error Saving Form Data:', ['exception' => $e]);
                 $this->messageManager->addErrorMessage(__('Unable to save form data.'));
             }
         }
 
         // Redirect back to the form page after submission
-        return $this->_redirect('*/*/');
+        return $this->_redirect('*/*/');  // Replace with the correct redirect URL if needed
     }
 }
+
