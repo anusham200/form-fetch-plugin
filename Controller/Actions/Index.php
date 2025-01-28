daca4fa519b0dc9a0f5f3c84e4c0af4139934f6a
anusham200 <141118850+anusham200@users.noreply.github.com>
2025-01-29 04:34:25 +0530

    Update Index.php

--- Controller/Actions/Index.php
+++ Controller/Actions/Index.php
@@ -1,3 +1,5 @@
+<?php
+
 namespace Custom\FormFetchPlugin\Controller\Actions;
 
 use Magento\Framework\App\Action\Action;
@@ -56,6 +58,8 @@ class Index extends Action
 
         // Return the same page without redirect
         $resultPage = $this->resultPageFactory->create();
+
+        // Set the page title dynamically
         $resultPage->getConfig()->getTitle()->set(__('Frontend Form Fetch Plugin'));
 
         return $resultPage;
@@ -93,30 +97,6 @@ class Index extends Action
             $connection->insert($tableName, $data);
         }
     }
-
-    public function fetchAction()
-    {
-        $email = $this->getRequest()->getPost('fetch_email');
-        $response = ['success' => false, 'message' => 'No email provided.'];
-
-        if ($email) {
-            // Query the database to fetch the details
-            $connection = $this->resource->getConnection();
-            $tableName = $this->resource->getTableName('form_fetch_plugin_data');
-            $userData = $connection->fetchRow(
-                "SELECT * FROM $tableName WHERE email = :email",
-                ['email' => $email]
-            );
-
-            if ($userData) {
-                $response = ['success' => true, 'data' => $userData];
-            } else {
-                $response = ['success' => false, 'message' => 'No data found for this email.'];
-            }
-        }
-
-        // Send the response as JSON
-        $this->getResponse()->setHeader('Content-Type', 'application/json');
-        $this->getResponse()->setBody(json_encode($response));
-    }
 }
+
+
