0a349f974a884ff3334b8053ccc1c6d070f34f40
anusham200 <141118850+anusham200@users.noreply.github.com>
2025-01-29 05:59:09 +0530

    Update Index.php

--- Controller/Actions/Index.php
+++ Controller/Actions/Index.php
@@ -50,7 +50,7 @@ class Index extends Action
 
                 // Add success message
                 $this->messageManager->addSuccessMessage(__('Data has been saved successfully.'));
-            } elseif (isset($params['fetch_email'])) {
+            } elseif (isset($params['fetch'])) {
                 // Handle fetch logic
                 if (empty($params['fetch_email'])) {
                     throw new \Exception(__('Email is required to fetch details.'));
