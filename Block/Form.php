8f636640d77d4827f9fe9c257fc9402c19934d0c
anusham200 <141118850+anusham200@users.noreply.github.com>
2025-01-29 08:49:44 +0530

    Update Form.php

--- Block/Form.php
+++ Block/Form.php
@@ -3,16 +3,18 @@ namespace Custom\FormFetchPlugin\Block;
 
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Message\ManagerInterface;
 
 class Form extends Template
 {
     protected $request;
     protected $messageManager;
+    protected $_fetchedData = null;
 
     public function __construct(
         Template\Context $context,
         RequestInterface $request,
-        \Magento\Framework\Message\ManagerInterface $messageManager,
+        ManagerInterface $messageManager,
         array $data = []
     ) {
         $this->request = $request;
@@ -20,28 +22,60 @@ class Form extends Template
         parent::__construct($context, $data);
     }
 
+    /**
+     * Get URL for form submission
+     */
     public function getFormActionUrl()
     {
-        return $this->getUrl('formfetch/actions/index');  // Adjust if necessary
+        return $this->getUrl('formfetch/actions/index');  // Submit form URL
     }
 
+    /**
+     * Get URL for fetching details
+     */
+    public function getFetchUrl()
+    {
+        return $this->getUrl('formfetch/actions/fetch'); // Fetch details URL
+    }
+
+    /**
+     * Get Magento Form Key HTML
+     */
     public function getFormKeyHtml()
     {
         return $this->getLayout()->createBlock('Magento\Framework\View\Element\FormKey')->getFormKeyHtml();
     }
 
+    /**
+     * Get Success Message
+     */
     public function getSuccessMessage()
-{
-    $messages = $this->messageManager->getMessages()->getItems();
+    {
+        $messages = $this->messageManager->getMessages()->getItems();
 
-    // Loop through messages and find the success message
-    foreach ($messages as $message) {
-        if ($message->getType() == \Magento\Framework\Message\MessageInterface::TYPE_SUCCESS) {
-            return $message->getText();
+        foreach ($messages as $message) {
+            if ($message->getType() == \Magento\Framework\Message\MessageInterface::TYPE_SUCCESS) {
+                return $message->getText();
+            }
         }
+
+        return ''; // No success message found
     }
 
-    return ''; // Return empty string if no success message found
-}
+    /**
+     * Set fetched data for display
+     */
+    public function setFetchedData($data)
+    {
+        $this->_fetchedData = $data;
+        return $this; // Return $this for chaining
+    }
 
+    /**
+     * Get fetched data
+     */
+    public function getFetchedData()
+    {
+        return $this->_fetchedData;
+    }
 }
